92e993f4bfcd55a0f6c47b7d56dc858658917625
ObiLadd2 <McToal-E@ulster.ac.uk>
2023-12-11 11:30:51 +0000

    yes

--- Assets/scripts/Damage.cs
+++ Assets/scripts/Damage.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public PlayerHealth pHealth;
-    public float damage;
+    public float damage = 20;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
